c8f40ebce22d711316391369c462bd32cf80e2c3
Callum Bondy <callum.bondy@gmail.com>
2025-08-22 15:18:18 -0700

    adding logo to reviews mobile

--- app/components/mobile/FriendTileMobile.jsx
+++ app/components/mobile/FriendTileMobile.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Logo from '../Logo';
 
 function FriendTileMobile({header, sub, content_header, content_sub}) {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -17,9 +18,9 @@ function FriendTileMobile({header, sub, content_header, content_sub}) {
       >
         {/* Logo */}
         <div className='flex gap-4'>
-          {/* <div className="flex-shrink-0">
-            <img src={MP} alt="MP logo" className="w-10 h-10" />
-          </div> */}
+          <div className="flex-shrink-0 w-[40px]">
+            <Logo></Logo>
+          </div>
           <div className="flex flex-col text-left">
             <span className="p-large-mobile uppercase text-black">
               {header}
